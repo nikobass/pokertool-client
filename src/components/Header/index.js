096cf9e4b4b90c51e7f511629064fdd0d40b0515
Juan5987 <juan.machado.5987@gmail.com>
2021-06-12 11:40:32 +0200

    add navlinks

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,31 +1,56 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+
 import './header.scss';
 
+
 const Header = () => (
-<header className="header">
+    <header className="header">
 
-    <div className="header__logo">
-        {/* En attente du logo */}
-        <img className="header__logo_img" src="#" alt="Logo" />
-        <span className="header__logo__title">Poker Tool</span>
-    </div>
+        <div className="header__logo">
+            {/* En attente du logo */}
+            <img className="header__logo_img" src="#" alt="Logo" />
+            <span className="header__logo__title">Poker Tool</span>
+        </div>
 
-    <nav className="header__navbar">
-        <ul className="header__navbar__list">
-            <li className="header__navbar__list__element">Accueil</li>
-            <li className="header__navbar__list__element">Mes tournois</li>
-            <li className="header__navbar__list__element">Mes jetons</li>
-            <li className="header__navbar__list__element">Répartiteur</li>
-            <li className="header__navbar__list__element">FAQ</li>
-            <li className="header__navbar__list__element">Mon profil</li>
-        </ul>
-    </nav>
+        <nav className="header__navbar">
+            <NavLink
+                to="/accueil"
+                className="header__navbar__element"
+                activeClassName="header__navbar__element--selected"
+            >Accueil</NavLink>
+            <NavLink 
+            to="/tournaments"
+            className="header__navbar__element"
+            activeClassName="header__navbar__element--selected"
+            >Mes tournois</NavLink>
+            <NavLink 
+            to="/chip"
+            className="header__navbar__element"
+            activeClassName="header__navbar__element--selected"
+            >Mes jetons</NavLink>
+            <NavLink 
+            to="/distributor"
+            className="header__navbar__element"
+            activeClassName="header__navbar__element--selected"
+            >Répartiteur</NavLink>
+            <NavLink 
+            to="/faq"
+            className="header__navbar__element"
+            activeClassName="header__navbar__element--selected"
+            >FAQ</NavLink>
+            <NavLink 
+            to="/profil"
+            className="header__navbar__element"
+            activeClassName="header__navbar__element--selected"
+            >Mon profil</NavLink>
+        </nav>
 
-    <div className="header__connexion">
-        <button className="header__connexion__button">Connexion</button>
-    </div>
+        <div className="header__connexion">
+            <button className="header__connexion__button">Connexion</button>
+        </div>
 
-</header>
+    </header >
 
 )
 
