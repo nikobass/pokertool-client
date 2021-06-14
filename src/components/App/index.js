c4fe7e7d212c028a300facb8b70b97fcefb893a7
Juan5987 <juan.machado.5987@gmail.com>
2021-06-14 17:20:38 +0200

    fix bugs

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,7 +3,7 @@ import React, { useEffect, setState } from 'react';
 import { Switch, Route, useLocation } from 'react-router-dom';
 import { withRouter } from 'react-router';
 
-import {resetProfilModif} from 'src/actions/user';
+import { resetProfilModif } from 'src/actions/user';
 
 // == Import
 import './styles.css';
@@ -11,8 +11,10 @@ import './styles.css';
 // == Composant
 import Header from 'src/components/Header';
 import Footer from 'src/components/Footer';
+import Faq from 'src/components/Faq';
 import Profil from 'src/components/Profil';
 import { useDispatch } from 'react-redux';
+
 
 const App = () => {
 
@@ -20,45 +22,46 @@ const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    if(currentLocation === '/profil'){
+    if (currentLocation === '/profil') {
       dispatch(resetProfilModif());
     }
   })
-  
-  return (
-  <div className="app">
 
-    {/* Le switch est provisoire. Il va falloir qu'on gère ça plus tard. Pour le moment, il sert à afficher la chaque page pour pouvoir tester nos composants */}
-    <Switch>
-      <Route exact path="/">
-        <Header />
-        <Footer />
-      </Route>
-      <Route exact path="/tournaments">
-        <Header />
-        <Footer />
-      </Route>
-      <Route exact path="/chip">
-        <Header />
-        <Footer />
-      </Route>
-      <Route exact path="/distributor">
-        <Header />
-        <Footer />
-      </Route>
-      <Route exact path="/faq">
-        <Header />
-        <Faq />
-        <Footer />
-      </Route>
-      <Route path="/profil">
-        <Header />
-        <Profil />
-        <Footer />
-      </Route>
-    </Switch>
-  </div>
-);
+  return (
+    <div className="app">
+
+      {/* Le switch est provisoire. Il va falloir qu'on gère ça plus tard. Pour le moment, il sert à afficher la chaque page pour pouvoir tester nos composants */}
+      <Switch>
+        <Route exact path="/">
+          <Header />
+          <Footer />
+        </Route>
+        <Route exact path="/tournaments">
+          <Header />
+          <Footer />
+        </Route>
+        <Route exact path="/chip">
+          <Header />
+          <Footer />
+        </Route>
+        <Route exact path="/distributor">
+          <Header />
+          <Footer />
+        </Route>
+        <Route exact path="/faq">
+          <Header />
+          <Faq />
+          <Footer />
+        </Route>
+        <Route path="/profil">
+          <Header />
+          <Profil />
+          <Footer />
+        </Route>
+      </Switch>
+    </div>
+  );
+};
 
 // == Export
 
