ed828d470c01f79d8c9210dcdf8f57e47fdc278d
Juan5987 <juan.machado.5987@gmail.com>
2021-06-15 11:02:04 +0200

    create actions in user. add action LOGIN_SUCCESS in reducer. Create middleware and add it in store

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,11 +1,13 @@
 export const CHANGE_OPEN_FORM = 'CHANGE_OPEN_FORM'
-
-
 export const TOGGLE_MODIFY_PROFIL = 'TOGGLE_MODIFY_PROFIL';
 export const CHANGE_INPUT_VALUE = 'CHANGE_INPUT_VALUE';
 export const RESET_PROFIL_MODIF = 'RESET_PROFIL_MODIF';
 export const SHOW_CONNECTION_MODAL = 'SHOW_CONNECTION_MODAL';
 export const HIDE_MODAL = 'HIDE_MODAL';
+export const SUBMIT_LOGIN = 'SUBMIT_LOGIN';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_ERROR = 'LOGIN_ERROR';
+
 
 export const toggleModifyProfil = () => ({
     type: TOGGLE_MODIFY_PROFIL
@@ -32,3 +34,19 @@ export const hideModal = () => ({
 export const changeOpenForm = () => ({
   type: CHANGE_OPEN_FORM
 })
+
+export const submitLogin = () => ({
+    type: SUBMIT_LOGIN,
+  });
+
+export const loginSuccess = (apiData) => ({
+    type: LOGIN_SUCCESS,
+    //a voir avec Pascal les données retournées par le back
+    nickname: apiData.username,
+    email: apiData.email,
+    token: apiData.token,
+});
+
+export const loginError = () => ({
+    //TODO: gérer l'erreur
+});
---
+++ src/middlewares/auth.js
@@ -0,0 +1,43 @@
+
+import axios from 'axios';
+
+import {
+    SUBMIT_LOGIN,
+    loginSuccess,
+} from 'src/actions/user';
+
+const authMiddleware = (store) => (next) => (action) => {
+    switch (action.type) {
+
+        case SUBMIT_LOGIN: {
+            //on récupère le state
+            const state = store.getState();
+
+            //On prépare la requête
+            const loginRequest = {
+                method: 'post',
+                //TODO: prévoir l'API_URL dans le .env
+                url: `${process.env.API_URL}/signin`,
+                data: {
+                    // A voir avec Pascal les données attendues côté back
+                    email: state.user.email,
+                    password: state.user.password,
+                },
+            };
+            //On éxécute la requete préparée ci-dessus
+            axios(loginRequest)
+                .then((response) => {
+                    // je stocke le token et le pseudo dans mon store
+                    store.dispatch(loginSuccess(response.data));
+                    // une fois que j'ai le token, je peux direct demander les favoris
+                    store.dispatch(getFavorites());
+                });
+            break;
+        }
+        default:
+            next(action);
+            break;
+    }
+};
+
+export default authMiddleware;
