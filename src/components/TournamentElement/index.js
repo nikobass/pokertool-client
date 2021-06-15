b89d92976a31844301a99a742ec7c39a5e168552
degregory-kevin <degregory.kevin@gmail.com>
2021-06-15 14:43:10 +0200

    page tournaments ok css todo

---
+++ src/components/TournamentElement/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Edit, Eye, Trash2 } from 'react-feather';
+import './tournamentElement.scss'
+
+const TournamentElement = ({
+  name,
+  location,
+  cashPrice,
+  nbPlayers,
+  maxPlayers,
+  buyIn,
+  statut,
+  date
+}) => {
+  return (
+    <div className="tournament--element">
+      <span>
+        {name}
+      </span>
+      <span>
+        {date}
+      </span>
+      <span>
+        {location}
+      </span>
+      <span>
+        {buyIn}
+      </span>
+      <span>
+        {cashPrice}
+      </span>
+      <span>
+        {statut}
+      </span>
+      <span>
+        {nbPlayers}/{maxPlayers}
+      </span>
+      <button className="modify-tournament">
+        <Eye />
+      </button>
+      <button className="modify-tournament">
+        <Edit />
+      </button>
+      <button className="modify-tournament">
+        <Trash2 />
+      </button>
+    </div>
+  );
+};
+
+export default TournamentElement;
