2d37db6d6e087eee2f3e52466e01a5ac3cbfa0a4
Juan5987 <juan.machado.5987@gmail.com>
2021-06-18 14:07:41 +0200

    commit before merge

--- src/components/Distributor/index.js
+++ src/components/Distributor/index.js
@@ -8,8 +8,12 @@ const Distributor = () => (
     <main className="distributor">
         <form className="distributor__form">
             <h2 className="distributor__form__subtitle">Répartiteur de jetons</h2>
+            <button className="distributor__form__importChips">Importer mes jetons</button>
             <div className="distributor__form__container">
                 <DistributorChipElement />
+                <DistributorChipElement />
+                <DistributorChipElement />
+                <button className="distributor__form__container__addChip">Ajouter un jeton</button>
             </div>
         </form>
     </main>
