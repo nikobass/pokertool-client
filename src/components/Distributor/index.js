d5dffc4729a0f4c1492d8f88d6e7e9834faa6c3d
Juan5987 <juan.machado.5987@gmail.com>
2021-06-19 13:14:20 +0200

    add inputs for tournament  infos

--- src/components/Distributor/index.js
+++ src/components/Distributor/index.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 import './distributor.scss';
 import DistributorChipElement from './DistributorChipElement';
 
-import {addChip} from 'src/actions/distributor';
+import { addChip } from 'src/actions/distributor';
 
 const Distributor = ({
     nbChips,
@@ -14,29 +14,56 @@ const Distributor = ({
 
     const children = [];
 
-    for(let i = 0; i < nbChips; i++) {
-        children.push(<DistributorChipElement key={i} index={i}/>)
+    for (let i = 0; i < nbChips; i++) {
+        children.push(<DistributorChipElement key={i} index={i} />)
     }
 
 
     return (
 
-    <main className="distributor">
-        <form className="distributor__form">
+        <main className="distributor">
+
             <h2 className="distributor__form__subtitle">Répartiteur de jetons</h2>
-            <button className="distributor__form__importChips">Importer mes jetons</button>
-            <div className="distributor__form__container">
-                {
-                    children.map(chip => chip)
-                }
-                <div className="distributor__form__container__buttons">
-                <button onClick={handleAddChip} className="distributor__form__container__buttons__button distributor__form__container__buttons__addChip">Ajouter un jeton</button>
+
+            <form className="distributor__form">
+                <div className="distributor__form__chips">
+                    <button className="distributor__form__importChips">Importer mes jetons</button>
+
+                    <div className="distributor__form__container">
+                        {
+                            children.map(chip => chip)
+                        }
+
+                        <div className="distributor__form__container__buttons">
+                            <button onClick={handleAddChip} className="distributor__form__container__buttons__button distributor__form__container__buttons__addChip">Ajouter un jeton</button>
+                        </div>
+
+                    </div>
+
+                    <button className="distributor__form__calculate">Lancer le répartiteur</button>
                 </div>
-            </div>
-            <button className="distributor__form__calculate">Lancer le répartiteur</button>
-        </form>
-    </main>
-)}
+                <div className="distributor__form__tournament">
+                    <div className="distributor__form__tournament__inputs">
+
+                        <div className="distributor__form__tournament__inputs__container">
+                            <label className="distributor__form__tournament__inputs__container__label">Joueurs</label>
+                            <input type="number" className="distributor__form__tournament__inputs__container__input" />
+                        </div>
+                        <div className="distributor__form__tournament__inputs__container">
+                            <label className="distributor__form__tournament__inputs__container__label">Tapis</label>
+                            <input type="number" className="distributor__form__tournament__inputs__container__input" />
+                        </div>
+                        <div className="distributor__form__tournament__inputs__container">
+                            <label className="distributor__form__tournament__inputs__container__label">Blinds étape 1</label>
+                            <input type="number" className="distributor__form__tournament__inputs__container__input" />
+                        </div>
+                    </div>
+                </div>
+
+            </form>
+        </main>
+    )
+}
 
 Distributor.propTypes = {
     nbChips: PropTypes.number.isRequired,
