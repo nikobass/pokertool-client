06d1f014aa39e738b6ad0f5cc2a9cc184c830f75
Alexandre Kelifa <alexandre.kelifa@hotmail.com>
2021-07-01 12:07:45 +0200

    Home image scrollbars small fixes

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -6,7 +6,7 @@ import Card from 'src/components/Card';
 import './cards.scss'
 import chip from 'src/images/chips.jpg';
 import tournament from 'src/images/tournoi.jpg';
-import timer from 'src/images/timer.jpg';
+import timer from 'src/images/squared_timer.png';
 
 
 const Cards = () => {
@@ -16,15 +16,16 @@ const Cards = () => {
       title:'Gérer vos Tournois',
       description: 'Créez vos propres tournois en les paramétrant entièrement: Nom, Date, Lieu, Cash-Price, Tapis de départ, Blinds etc... !!!',
     },
-    {
-      image:chip,
-      title:'Gérer vos Jetons',
-      description:'Virtualisez les jetons de votre propre mallette de poker ! Choisissez leur couleur, leur valeur et le nombre.',
-    },
+
     {
       image:timer,
       title:'Gérer votre Timer',
       description:'Le timer est un outil qui permet aux joueurs de connaître les diverses informations indispensables au bon déroulement d’une partie.',
+    },
+    {
+      image:chip,
+      title:'Gérer vos Jetons',
+      description:'Virtualisez les jetons de votre propre mallette de poker ! Choisissez leur couleur, leur valeur et le nombre.',
     }
   ]
   
